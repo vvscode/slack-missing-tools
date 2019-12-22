96b385dd1211319e15c03cf80658cc8e4a8ee009
Vasil Vanchuk <v.vanchuk@tut.by>
2019-12-22 10:34:53 +0200

    Setup basic core

---
+++ commands/dump-users/index.js
@@ -0,0 +1,8 @@
+module.exports = (program) => {
+  program
+    .command('dump-users')
+    .description('Dump user information')
+    .action((...args) => {
+      console.log('dump users command', args);
+    });
+}
